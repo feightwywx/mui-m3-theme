7a61386a804adc5ef12d985708b245ffdf938da6
feightwywx <kururinmiracle@outlook.com>
2022-10-28 00:07:54 +0800

    add: Fab

---
+++ src/components/Fab/Fab.stories.tsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import {
+  Box,
+  CssBaseline,
+  Grid,
+  Typography,
+  Stack,
+  ThemeProvider,
+} from '@mui/material';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ColorResult, RGBColor, SketchPicker } from 'react-color';
+import {
+  createM3Palette,
+  unstable_createMaterialDesign3Theme,
+} from '../../asset';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from './Fab';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'ReactComponentLibrary/Fab',
+  component: Fab,
+} as ComponentMeta<typeof Fab>;
+
+const FabPreviewTemplate: ComponentStory<typeof Fab> = (args) => {
+  const [hexColor, setHexColor] = React.useState<RGBColor>({
+    r: 103,
+    g: 80,
+    b: 164,
+    a: 1,
+  });
+  const handleChangeComplete = (color: ColorResult) => {
+    setHexColor(color.rgb);
+  };
+  const m3Palette = createM3Palette(hexColor);
+  const myTheme = unstable_createMaterialDesign3Theme(m3Palette);
+  return (
+    <ThemeProvider theme={myTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <SketchPicker
+              color={hexColor}
+              onChangeComplete={handleChangeComplete}
+            />
+          </Grid>
+
+          <Grid item>
+            <Stack direction="column" spacing={2}>
+              <Typography variant="h3" fontWeight="bold">
+                Fabs
+              </Typography>
+              <Stack direction="row" spacing={2}>
+                <Fab color="primary" size="large">
+                  <AddIcon />
+                </Fab>
+                <Fab color="primary" size="medium">
+                  <AddIcon />
+                </Fab>
+                <Fab variant="extended" color="primary">
+                  <AddIcon />
+                  Extended Fab
+                </Fab>
+                <Fab size="small" color="primary">
+                  <AddIcon />
+                </Fab>
+              </Stack>
+
+              <Stack direction="row" spacing={2}>
+                <Fab color="secondary" size="large">
+                  <AddIcon />
+                </Fab>
+                <Fab color="secondary" size="medium">
+                  <AddIcon />
+                </Fab>
+                <Fab color="secondary" variant="extended">
+                  <AddIcon />
+                  Extended Fab
+                </Fab>
+                <Fab color="secondary" size="small">
+                  <AddIcon />
+                </Fab>
+              </Stack>
+
+              <Stack direction="row" spacing={2}>
+                <Fab color="tertiary" size="large">
+                  <AddIcon />
+                </Fab>
+                <Fab color="tertiary" size="medium">
+                  <AddIcon />
+                </Fab>
+                <Fab color="tertiary" variant="extended">
+                  <AddIcon />
+                  Extended Fab
+                </Fab>
+                <Fab color="tertiary" size="small">
+                  <AddIcon />
+                </Fab>
+              </Stack>
+
+              <Stack direction="row" spacing={2}>
+                <Fab color="surface" size="large">
+                  <AddIcon />
+                </Fab>
+                <Fab color="surface" size="medium">
+                  <AddIcon />
+                </Fab>
+                <Fab color="surface" variant="extended">
+                  <AddIcon />
+                  Extended Fab
+                </Fab>
+                <Fab color="surface" size="small">
+                  <AddIcon />
+                </Fab>
+              </Stack>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export const FabView = FabPreviewTemplate.bind({});
---
+++ src/components/Fab/Fab.ts
@@ -0,0 +1,116 @@
+import { Fab as MuiFab, FabProps as MuiFabProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React from 'react';
+import { mixLayer } from '../../asset/functions';
+
+declare module '@mui/material/Fab' {
+  interface FabPropsColorOverrides {
+    tertiary: true;
+    surface: true;
+    success: false;
+    info: false;
+    warning: false;
+  }
+}
+
+const Fab = styled(MuiFab, {
+  name: 'M3Fab',
+  slot: 'Root',
+})(({ theme, variant, size, color }) => ({
+  boxShadow: theme.shadows[3],
+  // fab size
+  height: '56px',
+  width: '56px',
+  borderRadius: '16px',
+  '>.MuiSvgIcon-root': {
+    height: '24px',
+    width: '24px',
+  },
+  ...(size === 'large' && {
+    height: '96px',
+    width: '96px',
+    borderRadius: '28px',
+    '>.MuiSvgIcon-root': {
+      height: '36px',
+      width: '36px',
+    },
+  }),
+  ...(size === 'small' && {
+    height: '40px',
+    width: '40px',
+    borderRadius: '12px',
+  }),
+  // fab variant
+  ...(variant === 'extended' && {
+    height: '56px',
+    minWidth: '80px',
+    width: 'auto',
+    padding: '16px',
+    '>.MuiSvgIcon-root': {
+      marginRight: '8px',
+    },
+  }),
+  // fab color
+  backgroundColor: theme.palette.primary.container,
+  color: theme.palette.primary.onContainer,
+  '&:hover': {
+    boxShadow: theme.shadows[4],
+    backgroundColor: mixLayer(
+      theme.palette.primary.container,
+      theme.palette.primary.onContainer,
+      0.08
+    ),
+  },
+  '&:active': {
+    boxShadow: theme.shadows[3],
+    backgroundColor: mixLayer(
+      theme.palette.primary.container,
+      theme.palette.primary.onContainer,
+      0.12
+    ),
+  },
+  ...((color === 'primary' ||
+    color === 'secondary' ||
+    color === 'tertiary') && {
+    backgroundColor: theme.palette[color].container,
+    color: theme.palette[color].onContainer,
+    '&:hover': {
+      boxShadow: theme.shadows[4],
+      backgroundColor: mixLayer(
+        theme.palette[color].container,
+        theme.palette[color].onContainer,
+        0.08
+      ),
+    },
+    '&:active': {
+      boxShadow: theme.shadows[3],
+      backgroundColor: mixLayer(
+        theme.palette[color].container,
+        theme.palette[color].onContainer,
+        0.12
+      ),
+    },
+  }),
+  ...(color === 'surface' && {
+    backgroundColor: theme.palette.surface.main,
+    color: theme.palette.primary.main,
+    '&:hover': {
+      boxShadow: theme.shadows[4],
+      backgroundColor: mixLayer(
+        theme.palette.surface.main,
+        theme.palette.primary.onContainer,
+        0.08
+      ),
+    },
+    '&:active': {
+      boxShadow: theme.shadows[3],
+      backgroundColor: mixLayer(
+        theme.palette.surface.main,
+        theme.palette.surface.on,
+        0.12
+      ),
+    },
+  }),
+})) as React.ComponentType<MuiFabProps>;
+
+export default Fab;
