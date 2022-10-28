b66bf7278e31f91905cb2c05b260323b5ce0f582
feightwywx <kururinmiracle@outlook.com>
2022-10-29 00:14:00 +0800

    add: NavRailItem :hover :active effect

--- src/components/NavigationRail/NavigationRailItem.tsx
+++ src/components/NavigationRail/NavigationRailItem.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/require-default-props */
-import { Box, useTheme } from '@mui/material';
+import { Box, styled, useTheme } from '@mui/material';
+import chroma from 'chroma-js';
 import React from 'react';
+import { mixLayer } from '../../asset/functions';
 
 function NavigationRailItem({
   icon,
@@ -15,40 +17,55 @@ function NavigationRailItem({
 }) {
   const theme = useTheme();
 
+  const RailItemBox = styled(Box)({
+    backgroundColor: 'none',
+    height: '56px',
+    textAlign: 'center',
+    color: selected
+      ? theme.palette.surface.on
+      : theme.palette.surface.onVariant,
+    fontSize: '12px',
+    lineHeight: '16px',
+    fontWeight: 500,
+    cursor: 'pointer',
+    '>.iconDiv': {
+      transition: '0.3s',
+      height: '32px',
+      width: '56px',
+      display: 'table-cell',
+      marginRight: '12px',
+      verticalAlign: 'middle',
+      borderRadius: '16px',
+      backgroundColor: selected ? theme.palette.secondary.container : 'none',
+      textAlign: 'center',
+      lineHeight: '0px',
+    },
+    '&:hover': {
+      fontWeight: 800,
+    },
+    '&:hover >.iconDiv': {
+      backgroundColor: selected
+        ? mixLayer(
+            theme.palette.secondary.container,
+            theme.palette.surface.on,
+            0.08
+          )
+        : chroma(theme.palette.surface.onVariant).alpha(0.08).css(),
+    },
+    '&:active >.iconDiv': {
+      backgroundColor: selected
+        ? mixLayer(
+            theme.palette.secondary.container,
+            theme.palette.surface.on,
+            0.12
+          )
+        : chroma(theme.palette.surface.onVariant).alpha(0.12).css(),
+    },
+  });
+
   return (
-    <Box
-      style={{
-        backgroundColor: 'none',
-        height: '56px',
-        textAlign: 'center',
-        color: selected
-          ? theme.palette.surface.on
-          : theme.palette.surface.onVariant,
-        fontSize: '12px',
-        lineHeight: '16px',
-        fontWeight: 500,
-        cursor: 'pointer',
-      }}
-      {...props}
-    >
-      <div
-        style={{
-          transition: '0.3s',
-          height: '32px',
-          width: '56px',
-          display: 'table-cell',
-          marginRight: '12px',
-          verticalAlign: 'middle',
-          borderRadius: '16px',
-          backgroundColor: selected
-            ? theme.palette.secondary.container
-            : 'none',
-          textAlign: 'center',
-          lineHeight: '0px',
-        }}
-      >
-        {icon}
-      </div>
+    <RailItemBox {...props}>
+      <div className="iconDiv">{icon}</div>
       <div
         style={{
           marginTop: '4px',
@@ -60,7 +77,7 @@ function NavigationRailItem({
       >
         {label}
       </div>
-    </Box>
+    </RailItemBox>
   );
 }
 
